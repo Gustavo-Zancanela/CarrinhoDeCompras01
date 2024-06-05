18cf2af42659e0bfa5a7c144ccd8533727700d01
ALUNO <ALUNO@LAB04-PC32>
2024-06-05 20:33:06 -0300

    Commit project CarrinhoDeCompras01

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,18 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName;
+        String itemDesc;
+        String message;
+        
+        custName = "Alex";
+        itemDesc = "Camisa";
+        message = custName + " quer comprar uma " + itemDesc;
+        
+        // Assign the message variable 
+        System.out.println(message);
+        
+        // Print and run the code
+        
+    }
+}
